9e3979759eb05a83432c2d36286a0dae83f6df2e
rio <rio.game018@gmail.com>
2025-09-04 22:29:35 +0900

    fix: Correct basePath and assetPrefix for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,8 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/taglink-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/taglink-website/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/TagLinkOfficial' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/TagLinkOfficial/' : '',
 }
 
 export default nextConfig
